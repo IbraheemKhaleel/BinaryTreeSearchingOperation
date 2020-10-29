1ccb235ce63af113dd14456558fd591d56f32d44
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-29 08:55:35 +0530

    Added : code for testing the searching element 63 from the tree

--- test/java/com/bridgelabz/MyBinaryTreeTest/MyBinaryTreeTest.java
+++ test/java/com/bridgelabz/MyBinaryTreeTest/MyBinaryTreeTest.java
@@ -21,7 +21,25 @@ public class MyBinaryTreeTest {
 		myBinaryTree.add(67);
 		myBinaryTree.add(95);
 		int size = myBinaryTree.getSize();
-		System.out.println(size);
 		Assert.assertEquals(13, size);
+	}
+	@Test
+	public void givenNumber_WhenSearchedToBST_SHouldReturnTrue() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(11);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		myBinaryTree.add(95);
+		boolean result = myBinaryTree.search(63);
+		Assert.assertEquals(true, result);
 	}
 }
